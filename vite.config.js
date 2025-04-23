d899d98bc8796d54c9ef00c19b515ec6809ddea3
Harshit Pandey <70868493+harshitS2@users.noreply.github.com>
2025-04-23 18:37:07 +0530

    Update vite.config.js for access anywhere

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/todo-app-Local/', 
-})
+  base: '/todo-app-Local/',
+  server: {
+    host: true,    // Enables access via IP (0.0.0.0)
+    port: 5173     // Optional: customize as needed
+  },
+});
